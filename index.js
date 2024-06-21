2a5f48b7411361eb0793be80d369d089ae436ea9
moriyam-mohona <moriyammohona@gmail.com>
2024-06-21 22:46:05 +0600

    middleware

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://food-bridge-96dfc.web.app"],
